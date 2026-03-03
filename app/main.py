1dd3eea82ab3d581ef02303e95c7614c8ceed5cc
jmilind1234 <jmilind1234@gmail.com>
2026-03-03 21:12:50 +0530

    added support for CORS, via fastapi.middleware.cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 import app.models.test as test_schema
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=["*"],
+    allow_methods=["*"],
+)
+
 test_schema.Base.metadata.create_all(bind=engine)
+
 @app.get("/health")
 async def health():
     return {"status": "ok"}
